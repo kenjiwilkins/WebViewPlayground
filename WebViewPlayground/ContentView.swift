89158444020b7799b4f9c0d446a104bfb66f2aa1
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-10-17 21:59:00 +0900

    Allow http

--- WebViewPlayground/ContentView.swift
+++ WebViewPlayground/ContentView.swift
@@ -9,35 +9,36 @@ import SwiftUI
 import WebKit
 
 struct ContentView: View {
-    @State private var urlString = "https://www.apple.com"
-    @State private var currentURL = "https://www.apple.com"
+    @State private var urlString = "https://github.com"
+    @State private var currentURL = "https://github.com"
 
     var body: some View {
-        VStack(spacing: 0){
+        VStack(spacing: 0) {
             HStack {
                 TextField("Input URL", text: $urlString).textFieldStyle(
                     RoundedBorderTextFieldStyle()
-                ).autocapitalization(.none).disableAutocorrection(true).keyboardType(.URL)
-                Button("GO"){
+                ).keyboardType(.URL) // URLキーボードを使用
+                    .textContentType(.URL) // URL入力として認識
+                    .autocapitalization(.none) // 自動大文字変換をオフ
+                    .autocorrectionDisabled() // 自動修正をオフ
+                Button("GO") {
                     currentURL = urlString
                 }.padding(.leading, 8)
             }.padding()
             WebView(urlString: currentURL)
         }
-        
     }
-
 }
 
 struct WebView: UIViewRepresentable {
     let urlString: String
-    
-    func makeUIView(context: Context) -> WKWebView {
+
+    func makeUIView(context _: Context) -> WKWebView {
         let webView = WKWebView()
         return webView
     }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
+
+    func updateUIView(_ webView: WKWebView, context _: Context) {
         if let url = URL(string: urlString) {
             let request = URLRequest(url: url)
             webView.load(request)
--- WebViewPlaygroundTests/WebViewPlaygroundTests.swift
+++ WebViewPlaygroundTests/WebViewPlaygroundTests.swift
@@ -9,9 +9,7 @@ import Testing
 @testable import WebViewPlayground
 
 struct WebViewPlaygroundTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
